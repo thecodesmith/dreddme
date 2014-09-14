2c04a08fd9a00c61c5a31d242b8053e62c5e67ff
Brian Stewart <brian@thecodesmith.com>
2014-09-14 02:10:49 -0500

    Updated sample config.tcl

--- config.tcl
+++ config.tcl
@@ -2,4 +2,4 @@ set ::dredd::url   "http://dpl.cs.uwec.edu/operations/dredd/problem"
 
 # Note: Replace these with your ID
 set ::dredd::user  "stewarbc"
-set ::dredd::token "b1349de69020cbae7092d3586a67bc9e"
+set ::dredd::token "b1349-YOUR-AUTH-TOKEN-HERE-67bc9e"
