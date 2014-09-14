3cd21503665f7b6e7917ebbe355cc2ffb0211c22
Brian Stewart <brian@thecodesmith.com>
2014-09-14 02:08:28 -0500

    Added sample config.tcl

---
+++ config.tcl
@@ -0,0 +1,5 @@
+set ::dredd::url   "http://dpl.cs.uwec.edu/operations/dredd/problem"
+
+# Note: Replace these with your ID
+set ::dredd::user  "stewarbc"
+set ::dredd::token "b1349de69020cbae7092d3586a67bc9e"
